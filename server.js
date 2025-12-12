baf2fb7835de4ca96649329ed388e21d57b316f9
mslogisticbusiness-source <mslogisticbusiness@gmail.com>
2025-12-13 00:58:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("MS Logistic Backend Running");
-});// server placeholder
+});
+
+// IMPORTANT: Start Server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
