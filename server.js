98cb2f803d915a9eaf350aef0579bdda7ebce8de
mslogisticbusiness-source <mslogisticbusiness@gmail.com>
2025-12-15 20:35:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
-const authRoutes = require("./src/routes/auth");
+// ROUTES
+const authRoutes = require("./src/src/routes/auth");
 app.use("/auth", authRoutes);
 
-// Test route
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("MS Logistic Backend Running");
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
