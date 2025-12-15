1a9e167ee86968a1b5a38ae3442315af9710e8ea
mslogisticbusiness-source <mslogisticbusiness@gmail.com>
2025-12-15 19:39:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const path = require("path");
 const port = process.env.PORT || 3000;
 
+/* =======================
+   Middlewares
+======================= */
+app.use(cors());
+app.use(express.json());
+
+/* =======================
+   Routes Import
+======================= */
+const authRoutes = require("./src/src/routes/auth");
+const bharatRoutes = require("./src/src/routes/bharat");
+const nimbusRoutes = require("./src/src/routes/nimbus");
+
+/* =======================
+   Routes Use
+======================= */
 app.get("/", (req, res) => {
   res.send("MS Logistic Backend Running");
 });
 
-// IMPORTANT: Start Server
+app.use("/auth", authRoutes);
+app.use("/bharat", bharatRoutes);
+app.use("/nimbus", nimbusRoutes);
+
+/* =======================
+   Start Server
+======================= */
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
